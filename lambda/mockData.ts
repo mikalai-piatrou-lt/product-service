e7c2013b45b2964513c4b6a5e9b6ab8a17d8fe2b
Mikalai_Piatrou <piatrou.mikalai@gmail.com>
2025-04-21 19:17:07 +0300

    Improved Product structure

--- lambda/mockData.ts
+++ lambda/mockData.ts
@@ -1,7 +1,7 @@
 import { Product } from './mockData.types';
 
 export const products: Product[] = [
-    { id: '1', name: 'Product 1', price: 100 },
-    { id: '2', name: 'Product 2', price: 200 },
-    { id: '3', name: 'Product 3', price: 300 },
+    { id: '1', name: 'Product 1', description: 'Product 1 description', price: 100 },
+    { id: '2', name: 'Product 2', description: 'Product 2 description', price: 200 },
+    { id: '3', name: 'Product 3', description: 'Product 3 description', price: 300 },
 ];
